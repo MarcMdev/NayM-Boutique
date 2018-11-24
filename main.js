fa6f5b8f3bd207c3f7042525f08142df8127a771
MarcMDev <marcmiranda.dev@gmail.com>
2018-11-24 23:59:27 +0000

    finished the first try on conding the full-screen gallery viewing

--- main.js
+++ main.js
@@ -1,10 +1,50 @@
-const images = document.querySelectorAll('.frame img')
-const fullFrame = document.querySelector('#full-frame')
 
+// * FULL SCREEN GALLERY
+// selectors
+const imageArray = Array.from(document.querySelectorAll('.frame img'))
+const fullGallery = document.querySelector('.full-gallery');
+const fullImage  = document.querySelector('.full-gallery img')
+const leftArrow = document.querySelector('#slide-left')
+const rightArrow = document.querySelector('#slide-right')
+const cancelBtn = document.querySelector('#slide-close')
 
+// ! Controls the index of images to retrieve from the Array
+let indexCtrl
 
-images.forEach(node => node.addEventListener('click', () => console.log(node)))
+// toggle visibility class
+function galleryShow() {
+  indexCtrl = imageArray.indexOf(this);
+  fullGallery.classList.toggle('hidden')
+  fullImage.setAttribute('src', this.getAttribute('src'))
+}
 
-console.log(images)
+function nextImage() {
+  if (indexCtrl === (imageArray.length - 1)) {
+    indexCtrl = 0;
+    fullImage.setAttribute('src', imageArray[0].getAttribute('src'))
+  }else {
+    fullImage.setAttribute('src', imageArray[indexCtrl + 1].getAttribute('src')); 
+    indexCtrl += 1;
+  }
+};
 
+function previousImage() {
+  if (indexCtrl === 0) {
+    indexCtrl = (imageArray.length -1);
+    fullImage.setAttribute('src', imageArray[indexCtrl].getAttribute('src'))
+  }else {
+    fullImage.setAttribute('src', imageArray[indexCtrl - 1].getAttribute('src')); 
+    indexCtrl -= 1;
+  }
+};
+
+// EVENT LISTENERS
+// Cancel button
+cancelBtn.addEventListener('click', () => fullGallery.classList.toggle('hidden'));
+// Add a function to each Image to display the full-screen Gallery
+imageArray.forEach(item => item.addEventListener('click', galleryShow))
+// Slide Buttons
+rightArrow.addEventListener('click', nextImage);
+leftArrow.addEventListener('click', previousImage);
+/************************************************** */
 
